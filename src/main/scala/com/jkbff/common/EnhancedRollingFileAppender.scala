0ef1afe3d2789f99f30f930ba130938d6af04f74
Tyrence <email1@jkbff.com>
2014-02-04 01:37:21 -0700

    created EnhancedRollingFileAppender to allow RollOnStartup

---
+++ src/main/scala/com/jkbff/common/EnhancedRollingFileAppender.scala
@@ -0,0 +1,22 @@
+package com.jkbff.common
+
+import org.apache.log4j.RollingFileAppender
+
+class EnhancedRollingFileAppender extends RollingFileAppender {
+	protected var rollOnStartup = false
+	
+	override def activateOptions(): Unit = {
+		if (rollOnStartup) {
+			super.rollOver()
+		}
+		super.activateOptions()
+	}
+	
+	def getRollOnStartup(): Boolean = {
+		rollOnStartup
+	}
+	
+	def setRollOnStartup(rollOnStartup: Boolean): Unit = {
+		this.rollOnStartup = rollOnStartup
+	}
+}
