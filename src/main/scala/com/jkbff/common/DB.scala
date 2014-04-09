e2cc70ad80d467ca6704f83f36faf4fe66243fb4
Jason Wheeler <email1@jkbff.com>
2014-04-09 16:40:26 -0600

    added DB.call() to support stored procedures

--- src/main/scala/com/jkbff/common/DB.scala
+++ src/main/scala/com/jkbff/common/DB.scala
@@ -16,7 +16,7 @@ class DB(ds: DataSource) {
 			}
 		}
 	}
-	
+
 	def querySingle[T](sql: String, params: Seq[Any], rowMapper: ResultSet => T): Option[T] = {
 		using(connection.prepareStatement(sql)) { stmt =>
 			setParams(stmt, params)
@@ -29,29 +29,45 @@ class DB(ds: DataSource) {
 			}
 		}
 	}
-	
+
+	def call[T](sql: String, params: Seq[Any], rowMapper: ResultSet => T): List[T] = {
+		using(connection.prepareStatement(sql)) { stmt =>
+			setParams(stmt, params)
+			using(stmt.executeQuery()) { rs =>
+				new ResultSetIterator(rs).map(rowMapper).toList
+			}
+		}
+	}
+
+	def call[T](sql: String, params: Seq[Any]): Unit = {
+		using(connection.prepareCall(sql)) { stmt =>
+			setParams(stmt, params)
+			stmt.executeUpdate()
+		}
+	}
+
 	def query[T](sql: String, rowMapper: ResultSet => T): List[T] = {
 		query(sql, Seq(), rowMapper)
 	}
-	
+
 	def setParams(stmt: PreparedStatement, params: Seq[Any]): Unit = {
 		params.foldLeft(1) { (index, param) =>
 			stmt.setObject(index, param)
 			index + 1
 		}
 	}
-	
+
 	def update(sql: String, params: Seq[Any]): Int = {
 		using(connection.prepareStatement(sql)) { stmt =>
 			setParams(stmt, params)
 			stmt.executeUpdate()
 		}
 	}
-	
+
 	def update(sql: String): Int = {
 		update(sql, Seq())
 	}
-	
+
 	def transaction[T]()(op: => T): T = {
 		try {
 			connection.setAutoCommit(false)
@@ -66,24 +82,24 @@ class DB(ds: DataSource) {
 			connection.setAutoCommit(true)
 		}
 	}
-	
+
 	def startTransaction() {
 		connection.setAutoCommit(false)
 	}
-	
+
 	def commitTransaction() {
 		connection.commit()
 		connection.setAutoCommit(true)
 	}
-	
+
 	def rollbackTransaction() {
 		connection.rollback()
 	}
-	
+
 	def close(): Unit = {
 		connection.close()
 	}
-	
+
 	def getColumnLabels(rs: ResultSet): List[String] = {
 		val meta = rs.getMetaData()
 		(1 to meta.getColumnCount()).foldLeft(List[String]()) { (params, i) =>
