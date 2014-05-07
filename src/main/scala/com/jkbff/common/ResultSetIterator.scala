e1a76b4803fe16e384c53338041a4f6398885a46
Jason Wheeler <email1@jkbff.com>
2014-05-07 15:07:58 -0600

    move com.jkbff.common classes to scala-util project

--- src/main/scala/com/jkbff/common/ResultSetIterator.scala
+++
@@ -1,8 +0,0 @@
-package com.jkbff.common
-
-import java.sql.ResultSet
-
-class ResultSetIterator(rs: ResultSet) extends Iterator[ResultSet] {
-	override def hasNext() = rs.next
-	override def next() = rs
-}
